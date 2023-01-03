4dbbb72f4ae7d486d77bba45f019e2d2023ecb6f
Nursultan Zarlyk <nzarlyk@microsoft.com>
2023-01-03 19:17:29 +0100

    Test assert script

--- .github/workflows/scripts/assert-output.ps1
+++ .github/workflows/scripts/assert-output.ps1
@@ -1,19 +1,20 @@
 $ErrorActionPreference="SilentlyContinue"
 Stop-Transcript | out-null
 $ErrorActionPreference = "Continue"
-Start-Transcript -path .\console-output.txt -Append:$false
+Start-Transcript -path .\console-output.txt -Append:$false | out-null
 
 & .\con-app-aarch64.exe
 
-Stop-Transcript
+Stop-Transcript | out-null
 
 $output = Get-Content .\console-output.txt -Raw 
 
 if ($output.Contains('Hello world'))
 {
-    Write-Host "Test PASSED!"
+    Write-Host "`nTest PASSED!"
 }
 else 
 {
+    Write-Host "`nTest FAILED!"
     exit 1
 }
