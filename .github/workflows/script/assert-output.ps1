61f4ceed301a5ce9d81ccb8b3b3488e655dbbb06
Nursultan Zarlyk <nzarlyk@microsoft.com>
2023-01-03 18:30:25 +0100

    Test assert-output script

---
+++ .github/workflows/script/assert-output.ps1
@@ -0,0 +1,18 @@
+$ErrorActionPreference="SilentlyContinue"
+Stop-Transcript | out-null
+$ErrorActionPreference = "Continue"
+Start-Transcript -path .\console-output.txt -Append:$false
+
+& .\con-app-aarch64.exe
+
+Stop-Transcript
+
+$output = Get-Content .\console-output.txt -Raw 
+if( $output.Contains('Hello world1'))
+{
+    Write-Host "Test PASSED!"
+}
+else 
+{
+    exit 1
+}
