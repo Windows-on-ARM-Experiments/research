ee56262cef8f507f607ce9cc21efdaf77bc81a96
Nursultan Zarlyk <nursik@users.noreply.github.com>
2023-02-10 13:30:26 +0100

    Remove and fix LINK command. Change default branch
    Co-authored-by: Nursultan Zarlyk <nzarlyk@microsoft.com>

---
+++ .github/workflows/scripts/link.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -ex # stop bash script on error
+
+research_dir=$(pwd)
+
+cd "C:\Program Files\Microsoft Visual Studio\2022\Preview\VC\Tools\MSVC"
+cd $(ls -d */|tail -n 1)
+cd ./bin/Hostarm64/arm64/
+link_exe_path=$(pwd)/link.exe
+
+cd "C:\Program Files (x86)\Windows Kits\10\Lib"
+cd $(ls -d */|tail -n 1)
+cd ./um/arm64/
+kernel32_path=$(pwd)/kernel32.Lib
+
+cd $research_dir
+
+"$link_exe_path" con-app-aarch64.o "$kernel32_path" /SUBSYSTEM:CONSOLE
